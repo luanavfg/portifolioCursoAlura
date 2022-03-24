b2434785dd609fea4fa97c26174c226df82e1be1
luanavfg <101883667+luanavfg@users.noreply.github.com>
2022-03-23 22:52:40 -0300

    portifólio alura completo

---
+++ script.js
@@ -0,0 +1,5 @@
+function mudaTema() {
+  document.body.classList.toggle("dark")
+  <!-- com esse comando iremos alterar, de maneira alternada a class do body, de acordo com o comando onClick -->
+  
+}
